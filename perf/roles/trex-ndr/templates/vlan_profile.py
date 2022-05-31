rh-nfv-int/hw-offload-playbooks

commit 34b1bebee39fa3af0def6bab3ed05cac976718b4

perf: fine tune vlan and add comments
Signed-off-by: Robin Jarry <rjarry@redhat.com>

perf/roles/trex-ndr/templates/vlan_profile.py
@@ -5,23 +5,21 @@ from trex_stl_lib.api import *
 
 class Profile:
     def get_streams(self, direction=0, tunables=(), **kwargs):
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--vlan", type=int, default=int("{{vlan}}"))
-        args = parser.parse_args(tunables)
-
+        ip_left = "16.0.0.1"
+        port_left = 1025
+        ip_right = "48.0.0.1"
+        port_left = 12
         pkt = Ether()
-
         if direction == 0:
-            pkt /= IP(src="16.0.0.1", dst="48.0.0.1")
-            pkt /= UDP(dport=12, sport=1025)
+            pkt /= IP(src=ip_left, dst=ip_right)
+            pkt /= UDP(sport=port_left, dport=port_right)
 
         else:
-            pkt /= Dot1Q(prio=1, vlan=args.vlan)
-            pkt /= IP(src="48.0.0.1", dst="16.0.0.1")
-            pkt /= UDP(dport=1025, sport=12)
-
+            pkt = Ether(src=eth_right, dst=eth_left)
+            pkt /= Dot1Q(vlan=int("{{vlan}}"))
+            pkt /= IP(src=ip_right, dst=ip_left)
+            pkt /= UDP(sport=port_right, dport=port_left)
         pkt /= b"x" * 20
-
         return [STLStream(packet=STLPktBuilder(pkt), mode=STLTXCont())]
 
 

perf/ovs-vlan.yml
@@ -1,4 +1,14 @@
 ---
+# TOPOLOGY
+#
+# +------------+   no VLAN  +--------+
+# |      port0 ==============        |
+# | dut        |            |  tgen  |
+# |      port1 **************        |
+# +------------+    VLAN    +--------+
+#
+# see roles/ovs-vlan/tasks/main.yml for dut configuration
+# and roles/ovs-vlan/tasks/{dpdk,hwol}.yml for datapath specifics
 
 - hosts: dut
   vars:

perf/roles/ovs-vlan/tasks/dpdk.yml
@@ -1,5 +1,12 @@
 ---
 
+# PACKET FLOW (*** no VLAN, === VLAN)
+#
+#   patch-br1 ** br0 ** pf0 ******************
+#       *
+#       *
+#   patch-br0 ** br1 == pf1 ==================
+
 - name: add patch port
   command: >
     ovs-vsctl add-port br0 patch-br1 --

perf/roles/ovs-vlan/tasks/hwol.yml
@@ -1,5 +1,12 @@
 ---
 
+# PACKET FLOW (*** no VLAN, === VLAN)
+#
+#   +-----+ vf0 ** vf0-rep ** br0 ** pf0 ******************
+#   |test-|
+#   | pmd |
+#   +-----+ vf1 ** vf1-rep ** br1 == pf1 ==================
+
 - name: put representor ports in their respective briges
   shell: |
     set -xe

perf/roles/ovs-vlan/tasks/main.yml
@@ -6,6 +6,7 @@
 
 - name: reset flows
   command: ovs-ofctl del-flows br1
+  tags: setup
 
 - name: configure flows
   command:

commit 1bcdae22ed870f1b00b215a6496737eed50faedf

perf: fine tune vlan and add comments (fix)
Fix typo.

Fixes: 34b1bebee39f ("perf: fine tune vlan and add comments")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

perf/roles/trex-ndr/templates/vlan_profile.py
@@ -8,14 +8,13 @@ class Profile:
         ip_left = "16.0.0.1"
         port_left = 1025
         ip_right = "48.0.0.1"
-        port_left = 12
+        port_right = 12
         pkt = Ether()
         if direction == 0:
             pkt /= IP(src=ip_left, dst=ip_right)
             pkt /= UDP(sport=port_left, dport=port_right)
 
         else:
-            pkt = Ether(src=eth_right, dst=eth_left)
             pkt /= Dot1Q(vlan=int("{{vlan}}"))
             pkt /= IP(src=ip_right, dst=ip_left)
             pkt /= UDP(sport=port_right, dport=port_left)